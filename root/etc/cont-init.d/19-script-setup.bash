310257fa52bfe8ccf1eac4c9709f6922248a9900
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2021-10-03 17:04:30 -0400

    Create 19-script-setup.bash

---
+++ root/etc/cont-init.d/19-script-setup.bash
@@ -0,0 +1,43 @@
+#!/usr/bin/with-contenv bash
+
+# create scripts directory if missing
+if [ ! -d "/config/scripts" ]; then
+	mkdir -p "/config/scripts"
+else
+	echo "Updating scripts..."
+	rm -rf /config/scripts/*
+fi
+
+if [ -d "/config/scripts" ]; then
+	cp /scripts/* /config/scripts/
+fi
+
+# create cache directory if missing
+if [ ! -d "/config/cache" ]; then
+	mkdir -p "/config/cache"
+fi
+
+# create logs directory if missing
+if [ ! -d "/config/logs" ]; then
+	mkdir -p "/config/logs"
+fi
+
+# create list directory if missing
+if [ ! -d "/config/list" ]; then
+	mkdir -p "/config/list"
+fi
+
+# create list directory if missing
+if [ ! -d "/config/ignore" ]; then
+	mkdir -p "/config/ignore"
+fi
+
+# set permissions
+chown -R abc:abc "/config"
+chown -R abc:abc "/scripts"
+chmod 777 -R "/scripts"
+chmod 777 -R "/config"
+
+echo "Complete..."
+
+exit $?
