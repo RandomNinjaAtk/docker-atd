db9fb00efaeac5b14d730ea72233b9c4d009e392
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2021-10-31 21:19:54 -0400

    New ClientConfigCheck

--- root/scripts/resources/download.sh
+++ root/scripts/resources/download.sh
@@ -1,3 +1,60 @@
+ClientConfigCheck () {
+	
+	#check existing config file
+	if [ -f /root/.config/streamrip/config.toml ]; then
+		if cat /root/.config/streamrip/config.toml | grep "/downloads-atd/temp" | read; then
+			log "TIDAL :: Existing config file found with the correct download location"
+		else
+			log "TIDAL :: ERROR :: Existing config file found with the wrong location, removing to import new config"
+			rm -rf "/root/.config/streamrip"
+		fi
+	fi
+
+	# create streamrip config directory if missing
+	if [ ! -d "/root/.config/streamrip" ]; then
+		mkdir -p "/root/.config/streamrip"
+		# check for backup token and use it if exists
+		if [ ! -f /root/.config/streamrip/config.toml ]; then
+			if [ -f /config/backup/streamrip_config.toml ]; then
+				log "TIDAL :: Importing backup config from \"/config/backup/streamrip_config.toml\""
+				cp -p /config/backup/streamrip_config.toml /root/.config/streamrip/config.toml
+				# remove backup token
+				rm /config/backup/streamrip_config.toml 
+			else
+				log "TIDAL :: No default config found, importing default config from \"$SCRIPT_DIR/streamrip_config.toml\""
+				if [ -f "$SCRIPT_DIR/streamrip_config.toml" ]; then
+					cp "$SCRIPT_DIR/streamrip_config.toml" /root/.config/streamrip/config.toml
+					chmod 777 -R /root
+				fi
+			fi
+		fi
+	fi
+	
+	TokenCheck=$(cat /root/.config/streamrip/config.toml | grep token_expiry | wc -m)
+	if [ $TokenCheck == 18 ]; then
+		log "TIDAL :: ERROR :: Loading client for required authentication, please authenticate, then exit the client..."
+		rip config --tidal
+	fi
+
+	if [ -f /root/.config/streamrip/config.toml ]; then
+		if [[ $(find "/root/.config/streamrip/config.toml" -mtime +6 -print) ]]; then
+			log "TIDAL :: ERROR :: Token expired, removing..."
+			rip config --tidal
+		else
+			# create backup of token to allow for container updates
+			if [ ! -d /config/backup ]; then
+				mkdir -p /config/backup
+			else
+				rm -rf /config/backup
+				mkdir -p /config/backup
+			fi
+			log "TIDAL :: Backing up config from \"/root/.config/streamrip/config.toml\" to \"/config/backup/streamrip_config.toml\""
+			cp -p /root/.config/streamrip/config.toml /config/backup/streamrip_config.toml
+		fi
+	fi
+
+}
+
 ClientSelfTest () {
 	log "SELF TEST :: PERFORMING DL CLIENT TEST"
 	ClientDownloadMusic "--max-quality 0 https://tidal.com/browse/track/234794"
